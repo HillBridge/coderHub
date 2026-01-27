e40b1c754a6d4691a1a776ecf4fcbbd90e21fb0e
qiaoguosheng@pagsmile.com <294238921@qq.com>
2026-01-27 10:57:05 +0800

    通过子查询方式获取动态下的评论总数量

--- src/service/momentService.js
+++ src/service/momentService.js
@@ -18,7 +18,10 @@ class MomentService {
   }
 
   async getMomentList({ pageNum, pageSize }) {
-    const statement = `${sqlFragment} LIMIT ?, ?`;
+    // 子查询: 查询每个动态的评论数量
+    const statement = `SELECT m.id id, m.content content, m.createTime createTime, JSON_OBJECT('userId', u.id, 'userName', u.username) userInfo, 
+      (SELECT COUNT(*) FROM comments c WHERE c.moment_id = m.id) commentCount
+      FROM moments m LEFT JOIN users u ON m.user_id = u.id LIMIT ?, ?`;
     const [result] = await pool.execute(statement, [pageNum, pageSize]);
     return result;
   }
